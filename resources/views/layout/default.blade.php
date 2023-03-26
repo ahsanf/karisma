a4cf2c396ca6d265f42dbe0428ab69a36a384b4e
ahsanf <ahsan.firdaus@spesolution.com>
2023-03-26 11:55:50 +0700

    feat: initialize repository

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('dz.name') }} | @yield('title', $data['page_title'] ?? '')</title>
+
+	<meta name="description" content="@yield('page_description', $data['page_description'] ?? '')"/>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon.png') }}">
+
+
+
+	@foreach ($data['action'] as $key => $data['action'] )
+    @if(!empty(config('dz.public.pagelevel.css.'.$data['action'])))
+    @foreach(config('dz.public.pagelevel.css.'.$data['action']) as $style)
+    <link href="{{ asset($style) }}" rel="stylesheet" type="text/css"/>
+    @endforeach
+    @endif
+    @endforeach
+
+	{{-- Global Theme Styles (used by all pages) --}}
+    @if(!empty(config('dz.public.global.css')))
+    @foreach(config('dz.public.global.css') as $style)
+    <link href="{{ asset($style) }}" rel="stylesheet" type="text/css"/>
+    @endforeach
+    @endif
+
+    @if (!empty(config('dz.public.pagelevel.css.uc_toastr')))
+    @foreach (config('dz.public.pagelevel.css.uc_toastr') as $style)
+    <link href="{{ asset($style) }}" rel="stylesheet" type="text/css" />
+    @endforeach
+    @endif
+
+
+</head>
+
+<body>
+
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
+
+        <!--**********************************
+            Nav header start
+        ***********************************-->
+        <div class="nav-header">
+            <a href="{!! url('/index') !!}" class="brand-logo">
+			@if(!empty($data['logo']))
+				<img class="logo-abbr" src="{{ asset($data['logo']) }}" alt="">
+			@else
+                <img class="logo-abbr" src="{{ asset('images/logo.png') }}" alt="">
+			@endif
+			@if(!empty($data['logoText']))
+                <img class="logo-compact" src="{{ asset($data['logoText']) }}" alt="">
+                <img class="brand-title" src="{{ asset($data['logoText']) }}" alt="">
+			@else
+                <img class="logo-compact" src="{{ asset('images/logo-text.png') }}" alt="">
+                <img class="brand-title" src="{{ asset('images/logo-text.png') }}" alt="">
+			@endif
+            </a>
+
+            <div class="nav-control">
+                <div class="hamburger">
+                    <span class="line"></span><span class="line"></span><span class="line"></span>
+                </div>
+            </div>
+        </div>
+        <!--**********************************
+            Nav header end
+        ***********************************-->
+
+        <!--**********************************
+            Header start
+        ***********************************-->
+
+		@include('elements.header')
+
+
+        <!--**********************************
+            Header end ti-comment-alt
+        ***********************************-->
+
+        <!--**********************************
+            Sidebar start
+        ***********************************-->
+        @include('elements.sidebar')
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
+
+
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <!-- row -->
+            @yield('content')
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+
+
+        <!--**********************************
+            Footer start
+        ***********************************-->
+
+		@include('elements.footer')
+
+        <!--**********************************
+            Footer end
+        ***********************************-->
+
+		<!--**********************************
+           Support ticket button start
+        ***********************************-->
+
+        <!--**********************************
+           Support ticket button end
+        ***********************************-->
+
+
+    </div>
+    <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+	@include('elements.footer-scripts')
+    {!! Toastr::message() !!}
+</body>
+</html>
