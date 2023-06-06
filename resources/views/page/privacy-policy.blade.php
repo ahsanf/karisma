a24451cb209a8411fe37b96438672cb2649a67ae
ahsanf <ahsanfirdaus.2018@student.uny.ac.id>
2023-06-06 20:33:13 +0700

    feat(master): add privacy policy

---
+++ resources/views/page/privacy-policy.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Karisma - Privacy Policy</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="mt-4 mb-4">Privacy Policy</h1>
+
+        <p>
+            This Privacy Policy governs the manner in which Karisma collects, uses, maintains, and discloses information
+            collected from users (each, a "User") of the Karisma mobile application ("App"). This privacy policy applies to
+            the App and all products and services offered by Karisma.
+        </p>
+
+        <h4>Personal Identification Information</h4>
+        <p>
+            We may collect personal identification information from Users in a variety of ways, including, but not limited
+            to, when Users interact with our App, register on the App, subscribe to the newsletter, respond to a survey,
+            fill out a form, and in connection with other activities, services, features or resources we make available in
+            our App. Users may be asked for, as appropriate, name, email address, phone number, and other personal
+            information.
+        </p>
+
+        <h4>Non-personal Identification Information</h4>
+        <p>
+            We may collect non-personal identification information about Users whenever they interact with our App. Non-
+            personal identification information may include the browser name, the type of device, and technical information
+            about Users' means of connection to our App, such as the operating system and the Internet service providers
+            utilized and other similar information.
+        </p>
+
+        <h4>How We Use Collected Information</h4>
+        <p>
+            Karisma may collect and use Users' personal information for the following purposes:
+            <ul>
+                <li>To personalize user experience</li>
+                <li>To improve our App</li>
+                <li>To send periodic emails</li>
+            </ul>
+        </p>
+
+        <h4>How We Protect Your Information</h4>
+        <p>
+            We adopt appropriate data collection, storage, and processing practices and security measures to protect
+            against unauthorized access, alteration, disclosure, or destruction of your personal information, username,
+            password, transaction information, and data stored in our App.
+        </p>
+
+        <h4>Changes to This Privacy Policy</h4>
+        <p>
+            Karisma has the discretion to update this privacy policy at any time. When we do, we will revise the updated
+            date at the bottom of this page. We encourage Users to frequently check this page for any changes to stay
+            informed about how we are helping to protect the personal information we collect. You acknowledge and agree
+            that it is your responsibility to review this privacy policy periodically and become aware of modifications.
+        </p>
+
+        <h4>Your Acceptance of These Terms</h4>
+        <p>
+            By using this App, you signify your acceptance of this policy. If you do not agree to this policy, please do
+            not use our App. Your continued use of the App following the posting of changes to this policy will be deemed
+            your acceptance of those changes.
+        </p>
+
+        <h4>Contacting Us</h4>
+        <p>
+            If you have any questions about this Privacy Policy, the practices of this App, or your dealings with this App,
+            please contact us at:
+            <br>
+            <strong>Karisma App</strong>
+            <br>
+            Yogyakarta
+            <br>
+            social.ahsanf@gmail.com
+        </p>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
