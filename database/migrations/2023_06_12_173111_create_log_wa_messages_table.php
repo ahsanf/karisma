5811c14be7d99418ab83f17a9feb88309998974a
ahsanf <ahsan.firdaus@spesolution.com>
2023-06-13 22:27:50 +0700

    feat(master): add log wa

---
+++ database/migrations/2023_06_12_173111_create_log_wa_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_wa_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number_id', 50);
+            $table->string('form_number', 50);
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_wa_messages');
+    }
+};
