b0eec208a4cef0afbfa9ad1e7c55452057e94b69
ahsanf <ahsan.firdaus@spesolution.com>
2023-06-26 23:47:13 +0700

    feat: add bot api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\BotApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix'=> 'v1',
+    'as' => 'api.'
+], function(){
+    Route::get('/get-balance', [BotApiController::class, 'getBalance'])->name('get-balance');
 });
