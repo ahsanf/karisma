bc4ee3daaf45e064243b083c65494d95cabd9574
ahsanf <ahsan.firdaus@spesolution.com>
2024-03-29 11:00:31 +0700

    release-production: v3.0.0

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -4,13 +4,13 @@
     <div class="bg-white pt-4">
             <div class="container pt-5 pb-5">
                 <div class="row">
-                    <div class="col-md-6 d-flex flex-column align-items-start justify-content-center pb-2">
+                    <div class="col-md-6 col-xl-6 col-sm-12 col-xs-12 d-flex flex-column align-items-start justify-content-center pb-2">
                         <h2 class="linear-wipe">KELUARGA <br> REMAJA <br> MASJID AL MUTTAQIN</h2>
                         <p class="card-text mt-2">“The Feasibility Of Indonesian 4.0 To
                             Reach Our SDGs
                             Goals</p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-xl-6 col-sm-12 col-xs-12">
                         <img class="img-fluid" height="90%" src="{{ asset('landing/img/landing.png') }}" alt="">
                     </div>
                 </div>
@@ -23,7 +23,7 @@
             </div>
             <div class="container pt-5 pb-5">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-xl-6 col-sm-12 col-xs-12">
                         <h3 class="pt-2 pb-2"><b>Apa itu Karisma</b></h3>
                         <p>
                             Karisma (Keluarga Remaja Masjid Al Muttaqin) adalah organisasi pemuda non-profit di
@@ -32,7 +32,7 @@
                             tangguh, berkarakter, dan peduli terhadap lingkungan sekitar.
                         </p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-xl-6 col-sm-12 col-xs-12">
                         <img src="{{ asset('landing/img/video_opini.png') }}" alt="" class="img-fluid">
                     </div>
                 </div>
@@ -41,7 +41,7 @@
 
             <div class="container pt-5 pb-5">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 col-xl-6 col-sm-12 col-xs-12">
                         <img src="{{ asset('landing/img/poster.png') }}" alt="" class="img-fluid">
                     </div>
                     <div class="col-md-6">
